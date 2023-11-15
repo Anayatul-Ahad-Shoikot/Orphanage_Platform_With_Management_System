75a2f9f01244a6fcf66c256b06a0d9988be8d7f3
Anayatul Ahad Shoikot <aashoikot002@gmail.com>
2023-11-15 20:47:13 +0600

    .

--- Sources/my_db.sql
+++ Sources/my_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 14, 2023 at 04:02 PM
+-- Generation Time: Nov 15, 2023 at 03:46 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,20 @@ SET time_zone = "+00:00";
 --
 -- Database: `my_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_details`
+--
+
+CREATE TABLE `admin_details` (
+  `admin_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `priyority` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -55,14 +69,8 @@ CREATE TABLE `blog_actions` (
 --
 
 INSERT INTO `blog_actions` (`id`, `post_id`, `likes`, `comments`) VALUES
-(9, 39, 3, ''),
-(10, 40, 2, ''),
-(11, 41, 3, ''),
-(12, 42, 1, ''),
-(13, 43, 1, ''),
-(14, 44, 1, ''),
-(15, 45, 4, ''),
-(16, 46, 2, '');
+(17, 47, 2, ''),
+(18, 48, 1, '');
 
 -- --------------------------------------------------------
 
@@ -83,12 +91,10 @@ CREATE TABLE `blog_comment` (
 --
 
 INSERT INTO `blog_comment` (`comment_id`, `post_id`, `user_name`, `comments`, `date`) VALUES
-(4, 46, 'ela2', 'this is very good article', '2023-11-13 15:37:31'),
-(5, 46, 'ela2', 'wow nice', '2023-11-13 15:37:45'),
-(6, 45, 'ela', 'hello ', '2023-11-14 14:38:35'),
-(7, 45, 'ela', 'he he he', '2023-11-14 14:38:56'),
-(8, 45, 'ela', 'hi hi', '2023-11-14 14:41:57'),
-(9, 46, 'ela2', 'fgrg', '2023-11-14 14:42:23');
+(10, 47, 'ela2', 'HIII', '2023-11-15 03:20:11'),
+(11, 47, 'ela2', 'HELLOW', '2023-11-15 03:20:17'),
+(12, 47, 'ela2', 'dawdas', '2023-11-15 03:20:42'),
+(13, 48, 'ela2', 'qwe', '2023-11-15 14:38:43');
 
 -- --------------------------------------------------------
 
@@ -107,23 +113,9 @@ CREATE TABLE `blog_likes` (
 --
 
 INSERT INTO `blog_likes` (`like_id`, `post_id`, `viewer_username`) VALUES
-(18, 39, 'ahad'),
-(20, 40, 'ela'),
-(22, 41, 'ahad'),
-(23, 40, 'ahad'),
-(24, 42, 'ela'),
-(25, 41, 'ela'),
-(27, 44, 'ahad'),
-(28, 39, 'ela'),
-(29, 45, 'ela'),
-(31, 43, 'ela'),
-(32, 45, 'ahad'),
-(34, 39, 'ela2'),
-(35, 41, 'ela2'),
-(36, 46, 'ela2'),
-(37, 45, 'ela2'),
-(38, 45, 'sabrina2'),
-(39, 46, 'sabrina2');
+(40, 47, 'ela2'),
+(41, 47, 'sabrina2'),
+(42, 48, 'ela2');
 
 -- --------------------------------------------------------
 
@@ -146,8 +138,8 @@ CREATE TABLE `blog_post` (
 --
 
 INSERT INTO `blog_post` (`post_id`, `title`, `user_name`, `content`, `category`, `img_path`, `published`) VALUES
-(45, 'asdaw', 'ela', 'SADwd', 'old', 'Uploads2/399739997_917151276441523_6022247235894239656_n.jpg', '2023-11-08 11:04:18'),
-(46, 'Ela2 ela2 ela2 ela2 ela2', 'ela2', 'WDasdwDAFudfjhsnUGfSADUOFKJLGfopuegfjadshb:UEfo. AGFKJDABFCGASDUL;e>iGVF. SLKFVBGIEYVFkhlaCV.FCEHFGVSDFVBGIL;EU.KFGBKDV', 'child', 'img/Screenshot 2023-10-12 123940.png', '2023-11-13 15:16:15');
+(47, 'ahad shoikot', 'ela2', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.', 'child', 'img/Screenshot 2023-09-19 000957.png', '2023-11-15 04:19:53'),
+(48, 'qwert', 'ela2', 'qwe', 'old', 'img/Screenshot 2023-11-10 194906.png', '2023-11-15 15:38:38');
 
 -- --------------------------------------------------------
 
@@ -177,13 +169,9 @@ CREATE TABLE `org_details` (
 --
 
 INSERT INTO `org_details` (`org_id`, `org_name`, `user_name`, `description`, `contact_email`, `contact_phone`, `address`, `website`, `logo_path`, `established_date`, `location`, `mission_and_vision`, `reviews_testimonials`, `country`) VALUES
-(6, 'Child Zone Infinity', 'ela', 'it is the oldest charity. We a', 'ela@g.com', '0324325436', 'Madani avenew, Notun bazar, Dh', 'https://www.sabrina.com', 'Uploads/312308865_507861781367882_3667233183680698455_n.jpg', '1994-04-07', 'tanjum', 'Ensure the future ', '', 'Bangladesh'),
-(7, 'Ahnaf\'s Galaxy', 'ahnaf', 'it is the oldest charity. We a', 'ela@g.com', '032431232344', 'Madani avenew, Notun bazar, Dh', 'https://www.ahnaf.com', 'Uploads/011-201-128.jpg', '2320-02-13', 'Uttara - 2', 'To kill all childern', '', 'Bangladesh'),
-(8, 'Sabrinas Charity', 'sabrina', 'seves well, helps all. al chir', 'S_charity@gmail.com', '01123243134', 'Madani avenew, Notun bazar, Dh', 'https://www.s_charity.com', 'Uploads/0112320150.png', '0000-00-00', 'Uttara - 2', 'Serve all in once', '', 'Bangladesh'),
-(9, 'Sowrins Charity', 'sowrin', 'this is a test charity opening', 'scharity@gmail.com', '0123231453', 'Madani avenew, Notun bazar, Dh', 'https://www.s_charity.com', 'Uploads/R.jfif', '1992-03-13', 'Uttara - 2', 'this is a test charity opening', '', 'Bangladesh'),
 (12, 'Charity By Ela2', 'ela2', 'it is the oldest charity. We a', 'CharityByEla2@gmail.com', '01973336001', 'Madani avenew, Notun bazar, Dh', 'https://www.ela2Charity.com', 'img/Screenshot 2023-10-05 145951.png', '1985-07-17', 'Dhanmondi', 'To complete every child\'s wish', '', 'Bangladesh'),
-(13, '', 'admin', '', '', '', '', '', '', '0000-00-00', '', '', '', ''),
-(14, 'Sabrina Charity 123', 'sabrina2', 'We take care our childs like a mother', 'SC@gmail.com.org', '12345678910', 'mohakhali, dhaka - 1212', 'https://www.sab2Charity.com', 'img/Screenshot 2023-11-10 185814.png', '2023-03-09', 'Dhaka', 'To complete every child\'s wish', '', 'India');
+(14, 'Sabrina Charity 123', 'sabrina2', 'We take care our childs like a mother', 'SC@gmail.com.org', '12345678910', 'mohakhali, dhaka - 1212', 'https://www.sab2Charity.com', 'img/Screenshot 2023-11-10 185814.png', '2023-03-09', 'Dhaka', 'To complete every child\'s wish', '', 'India'),
+(15, 'shetu charity', 'shetu', 'it is the oldest charity. We all known as for our ', 'salim@gmail.com', '+8801973336001', 'mirpur -2', 'Charity By Ela2', 'img/Screenshot 2023-09-19 215724.png', '2023-11-11', 'Boshundhora , Dhaka - 1216', 'adswdaSD', '', 'Bangladesh');
 
 -- --------------------------------------------------------
 
@@ -193,7 +181,8 @@ INSERT INTO `org_details` (`org_id`, `org_name`, `user_name`, `description`, `co
 
 CREATE TABLE `orphan_profiles` (
   `orphan_id` int(11) NOT NULL,
-  `org_id` varchar(30) DEFAULT NULL,
+  `org_id` int(30) DEFAULT NULL,
+  `org_name` varchar(50) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `age` int(11) DEFAULT NULL,
@@ -222,11 +211,11 @@ CREATE TABLE `orphan_profiles` (
 -- Dumping data for table `orphan_profiles`
 --
 
-INSERT INTO `orphan_profiles` (`orphan_id`, `org_id`, `first_name`, `last_name`, `age`, `address`, `guardian_name`, `guardian_contact`, `gender`, `religion`, `date_of_birth`, `since`, `family_status`, `physical_condition`, `education_level`, `medical_history`, `hobby`, `favorite_food`, `favorite_games`, `skills`, `dreams`, `problems`, `other_comments`, `image_path`) VALUES
-(2, 'ela', 'sayma', 'Shetu', 14, 'mirpur-2', 'Shorifa', '019732843972', 'other', 'other', '0000-00-00', '2023-11-04', 'other', 'good', 'kindergarten', '', 'Crafts', 'fast Food', 'badminton', 'xyz', 'xyz', 'xyz', 'avarage', 'Uploads3/pic_1.jpg'),
-(4, 'ela', 'Shorifa', 'Rani', 9, 'mirpur -2', '', '', 'other', 'hindu', '0232-02-13', '2023-11-04', 'lost', 'mad', 'junior_high_school', 'none', 'swing', 'rice potato', 'kutkut', 'xyz', 'xyz', 'xyz', 'avarage', 'Uploads3/pic_3.jpg'),
-(5, 'ela', 'anayatul', 'ahad', 0, '', '', '', '', '', '0000-00-00', '2023-11-08', '', '', '', '', '', '', '', '', '', '', '', 'Uploads3/011212107_2.jpg'),
-(7, 'ela', 'Sabrina Zaman', 'Oishee', 22, 'Merul Badda', '--------------------------------------', '---------------', 'female', 'muslim', '2002-02-27', '2023-11-10', 'other', 'good', 'secondary_school', 'none', 'Thinking', 'unknown', 'Chess', 'none', 'A good House Wife ', 'overthinking ', 'Pretty ', 'Uploads3/sabrina.png');
+INSERT INTO `orphan_profiles` (`orphan_id`, `org_id`, `org_name`, `first_name`, `last_name`, `age`, `address`, `guardian_name`, `guardian_contact`, `gender`, `religion`, `date_of_birth`, `since`, `family_status`, `physical_condition`, `education_level`, `medical_history`, `hobby`, `favorite_food`, `favorite_games`, `skills`, `dreams`, `problems`, `other_comments`, `image_path`) VALUES
+(10, 14, 'sabrina2', 'asd', 'xyz', 12, 'asd', 'asd', 'asd', '', '', '0000-00-00', '2023-11-14', '', '', '', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'img/011-201-021.jpg'),
+(12, 14, 'sabrina2', 'Anayatul', 'Shoikot', 23, 'dadw', 'bahar uddin', '+8801973336001', 'female', 'cristian', '2023-11-11', '2023-11-15', 'unknow', 'disabled', 'primary_school', 'wad', 'awd', 'asdwd', 'asd', 'fyj', 'dfyjf', 'adf', 'Wjuyighjfy', 'img/Screenshot 2023-10-30 232034.png'),
+(13, 12, 'ela2', 'Shorifa', 'Rani', 25, 'mirpur -2', 'bahar uddin', '+8801973336001', 'female', 'buddha', '2023-11-13', '2023-11-15', 'unknow', 'deaf', 'senior_high_school', 'wad', 'awd', 'asdwd', 'asd', 'fyj', 'dfyjf', 'adf', 'Wjuyighjfy', 'img/Screenshot 2023-11-05 231333.png'),
+(16, 15, 'shetu', 'Anayatul', 'Shoikot', 0, 'Madani avenew, Notun bazar, Dhaka - 1216', '', '0324325436', '', '', '0000-00-00', '2023-11-15', '', '', '', '', '', '', '', '', '', '', '', 'img/Screenshot 2023-10-24 095707.png');
 
 -- --------------------------------------------------------
 
@@ -237,7 +226,8 @@ INSERT INTO `orphan_profiles` (`orphan_id`, `org_id`, `first_name`, `last_name`,
 CREATE TABLE `removed_orphans` (
   `si` int(11) NOT NULL,
   `orphan_id` int(11) NOT NULL,
-  `org_id` varchar(30) DEFAULT NULL,
+  `org_id` int(11) DEFAULT NULL,
+  `org_name` varchar(50) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `age` int(11) DEFAULT NULL,
@@ -266,9 +256,12 @@ CREATE TABLE `removed_orphans` (
 -- Dumping data for table `removed_orphans`
 --
 
-INSERT INTO `removed_orphans` (`si`, `orphan_id`, `org_id`, `first_name`, `last_name`, `age`, `address`, `guardian_name`, `guardian_contact`, `gender`, `religion`, `date_of_birth`, `since`, `family_status`, `physical_condition`, `education_level`, `medical_history`, `hobby`, `favorite_food`, `favorite_games`, `skills`, `dreams`, `problems`, `other_comments`, `image_path`) VALUES
-(1, 6, 'ela', 'tanjum', '', 0, '', '', '', '', '', '0000-00-00', '2023-11-08', '', '', '', '', '', '', '', '', '', '', '', 'Uploads3/011212107_2.jpg'),
-(2, 3, 'ela', 'Shorifa', 'Rani', 9, 'mirpur -2', '', '', 'female', 'muslim', '0123-02-12', '2023-11-04', 'other', 'mad', 'kindergarten', 'none', 'swing', 'rice potato', 'kutkut', '', '', '', '', 'Uploads/0112230549.png');
+INSERT INTO `removed_orphans` (`si`, `orphan_id`, `org_id`, `org_name`, `first_name`, `last_name`, `age`, `address`, `guardian_name`, `guardian_contact`, `gender`, `religion`, `date_of_birth`, `since`, `family_status`, `physical_condition`, `education_level`, `medical_history`, `hobby`, `favorite_food`, `favorite_games`, `skills`, `dreams`, `problems`, `other_comments`, `image_path`) VALUES
+(4, 9, 0, 'ela2', 'asd', 'xyz', 12, 'asd', 'asd', 'asd', 'female', 'hindu', '2023-11-11', '2023-11-14', 'unknow', 'autistic', 'junior_high_school', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'Uploads3/111-213-034.jpg'),
+(5, 11, 14, 'sabrina2', 'Anayatul', 'Shoikot', 0, '', '', '+8801973336001', '', '', '0000-00-00', '2023-11-15', 'other', '', '', 'wad', 'awd', 'Hadudu', '', '', '', '', '', 'img/Screenshot 2023-09-23 193732.png'),
+(6, 15, 12, 'ela2', 'Anayatul', 'Shoikot', 0, 'dadw', '', '+8801973336001', '', '', '0000-00-00', '2023-11-15', '', '', '', '', '', '', '', '', '', '', '', 'img/Screenshot 2023-09-29 090840.png'),
+(7, 14, 12, 'ela2', 'sabrina zaman', 'Rani', 11, 'mirpur -2', 'bahar uddin', '+8801973336001', '', '', '0000-00-00', '2023-11-15', '', '', '', 'wad', 'awd', 'asdwd', 'asd', 'fyj', 'dfyjf', 'adf', 'Wjuyighjfy', 'img/Screenshot 2023-10-26 225946.png'),
+(8, 17, 15, 'shetu', 'Shorifa', 'Rani', 0, 'mirpur -2', '', '+8801973336001', '', '', '0000-00-00', '2023-11-15', '', '', '', '', '', '', '', '', '', '', '', 'img/Screenshot 2023-11-08 102338.png');
 
 -- --------------------------------------------------------
 
@@ -290,12 +283,11 @@ CREATE TABLE `user_account` (
 --
 
 INSERT INTO `user_account` (`user_id`, `user_name`, `user_pass`, `user_email`, `acc_type`, `since`) VALUES
-(29, 'ahad', '1234', 'ahad@gmail.com', 'user', '2023-11-02 07:05:10'),
-(30, 'ela', 'abc123', 'ela@gmail.com', 'org', '2023-11-02 07:54:04'),
 (36, 'ahad3', '$2y$10$Zikd2g72i48KlYIGfRh3I.fQxuwSsbQwYS8kMrDM4xgvvhQ7envGW', 'aashoikot002@gmail.com', 'user', '2023-11-13 11:54:18'),
 (37, 'ela2', '$2y$10$YqAs/fajdTKen9wPtrcQv.Cj0.ns4H0Ydfb.DQ9y1oWHcFanESp6q', 'ela2@gmail.com', 'org', '2023-11-13 11:55:57'),
 (38, 'admin', '$2y$10$iVwuPBUbsEzjPvrChuN5FemjrzGTc0LIlACDGgr14av6y3VW6cxFy', 'admin@gmail.com', 'admin', '2023-11-13 11:58:59'),
-(39, 'sabrina2', '$2y$10$Kv0HzjPhb3D3owqS7cUSHu5b818mAwntx2FrzUWclGfiJvXR0sKFy', 's@gmail.com', 'org', '2023-11-14 15:39:35');
+(39, 'sabrina2', '$2y$10$Kv0HzjPhb3D3owqS7cUSHu5b818mAwntx2FrzUWclGfiJvXR0sKFy', 's@gmail.com', 'org', '2023-11-14 15:39:35'),
+(40, 'shetu', '$2y$10$KtKQjX9t0DG2cpXHIZFwc.AGOzbJ1U/me5IiydbhPImVx77onUWqm', 'shet', 'org', '2023-11-15 08:33:59');
 
 -- --------------------------------------------------------
 
@@ -325,12 +317,17 @@ CREATE TABLE `user_details` (
 --
 
 INSERT INTO `user_details` (`SI`, `full_name`, `user_name`, `birthdate`, `user_contact`, `gender`, `NID`, `address`, `website`, `work_name`, `work_add`, `area`, `country`, `image_path`) VALUES
-(15, 'Anayatul Ahad', 'ahad', '2000-11-30', '01973336001', 'Male', '********************', 'Madani avenew, Notun bazar, Dh', 'https://www.ahad.com', 'Student', 'gulshan - 2, dhaka 1212', 'mirpur-2', 'Bangladesh', 'Uploads/011-212-107.jpg'),
 (16, '', 'ahad3', '0000-00-00', '', '', '', '', '', '', '', '', '', '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin_details`
+--
+ALTER TABLE `admin_details`
+  ADD PRIMARY KEY (`admin_id`);
 
 --
 -- Indexes for table `adoptions`
@@ -399,6 +396,12 @@ ALTER TABLE `user_details`
 --
 
 --
+-- AUTO_INCREMENT for table `admin_details`
+--
+ALTER TABLE `admin_details`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `adoptions`
 --
 ALTER TABLE `adoptions`
@@ -408,49 +411,49 @@ ALTER TABLE `adoptions`
 -- AUTO_INCREMENT for table `blog_actions`
 --
 ALTER TABLE `blog_actions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `blog_comment`
 --
 ALTER TABLE `blog_comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `blog_likes`
 --
 ALTER TABLE `blog_likes`
-  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `blog_post`
 --
 ALTER TABLE `blog_post`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `org_details`
 --
 ALTER TABLE `org_details`
-  MODIFY `org_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `org_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `orphan_profiles`
 --
 ALTER TABLE `orphan_profiles`
-  MODIFY `orphan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `orphan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `removed_orphans`
 --
 ALTER TABLE `removed_orphans`
-  MODIFY `si` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `si` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user_account`
 --
 ALTER TABLE `user_account`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `user_details`
