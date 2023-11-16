fe6bf869f8415642ad94266f3795d8a29bcf40de
Anayatul Ahad Shoikot <aashoikot002@gmail.com>
2023-11-16 12:05:30 +0600

    done

--- Root/Orphanage/SEARCH_ORPHAN_BE.php
+++ Root/Orphanage/SEARCH_ORPHAN_BE.php
@@ -1,37 +1,36 @@
 <?php
-include('/xampp/htdocs/DBMS_Project_Organized_One/Includes/db_con.php');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['search'])) {
-    $search = mysqli_real_escape_string($con, $_POST['search']);
-    $query = "SELECT * FROM orphan_profiles 
-              WHERE first_name LIKE '%$search%' OR last_name LIKE '%$search%' 
-              OR age LIKE '%$search%' OR address LIKE '%$search%' 
-              OR gender LIKE '%$search%' OR religion LIKE '%$search%' 
-              OR physical_condition LIKE '%$search%' OR education_level LIKE '%$search%'
-              OR medical_history LIKE '%$search%'";
+    include('/xampp/htdocs/DBMS_Project_Organized_One/Includes/db_con.php');
 
-    $result = mysqli_query($con, $query);
-    if ($result) {
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo '<div class="card">';
-                echo '<div class="pb"  style="background-image: url(\'' . $row['image_path'] . '\');"></div>';
-                echo '<div class="info">';
-                echo '<h1>' . $row['first_name'] . '  ' . $row['last_name'] . '</h1>';
-                echo '</div>';
-                echo '<div class="buttons">';
-                echo '<a href="/Root/Orphanage/REMOVE_ORPHAN_BE.php?orphan_id=' . $row['orphan_id'] . '" class="message">Remove</a>';
-                echo '<a href="/Root/Orphanage/ORPHAN_PROFILE.php?orphan_id=' . $row['orphan_id'] . '" class="message"> View </a>';
-                echo '</div>';
-                echo '</div>';
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['query'])) {
+        $name = $_SESSION['user_name'];
+        $search = $_GET['query'];
+        if (!empty($search)) {
+            $query = "SELECT * FROM orphan_profiles WHERE (first_name LIKE '%$search%' OR last_name LIKE '%$search%' OR age LIKE '%$search%' OR address LIKE '%$search%' OR gender='$search' OR religion LIKE '%$search%' OR physical_condition LIKE '%$search%' OR education_level LIKE '%$search%' OR medical_history LIKE '%$search%') AND org_name = '$name'";
+            $result = mysqli_query($con, $query);
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo '<div class="card">';
+                    echo '<div class="pb"  style="background-image: url(\'' . $row['image_path'] . '\');"></div>';
+                    echo '<div class="info">';
+                    echo '<h1>' . $row['first_name'] . '  ' . $row['last_name'] . '</h1>';
+                    echo '</div>';
+                    echo '<div class="buttons">';
+                    echo '<a href="/Root/Orphanage/REMOVE_ORPHAN_BE.php?orphan_id=' . $row['orphan_id'] . '" class="message">Remove</a>';
+                    echo '<a href="/Root/Orphanage/ORPHAN_PROFILE.php?orphan_id=' . $row['orphan_id'] . '" class="message"> View </a>';
+                    echo '</div>';
+                    echo '</div>';
+                }
+            } else {
+                echo 'No Orphan Records found';
             }
         } else {
-            echo 'No Orphan Records found';
+            echo '<p>Please provide what you want to search.</p>';
         }
     } else {
-        echo 'Error executing the query: ' . mysqli_error($con);
+        header("Location: /Root/Orphanage/ORPHAN_DASH.php");
+        exit(0);
     }
-}
+    mysqli_close($con);
 
-mysqli_close($con);
 ?>
